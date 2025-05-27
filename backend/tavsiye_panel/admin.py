41fd90aa47668437167185845039d423dec04dfc
emir.dev <emircanbulut04@gmail.com>
2025-05-27 23:44:57 +0300

    ..

--- backend/tavsiye_panel/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import StockRecommendation
-
-@admin.register(StockRecommendation)
-class StockRecommendationAdmin(admin.ModelAdmin):
-    list_display = ('date', 'title', 'description', 'stock_symbols')
-    list_filter = ('date',)
-    search_fields = ('title', 'description', 'stock_symbols')
--- backend/tavsiye_panel/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TavsiyePanelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tavsiye_panel'
