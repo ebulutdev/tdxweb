a731a9d1f2ada53d7300ab3b0e9230ced6a76125
emir.dev <emircanbulut04@gmail.com>
2025-05-31 20:13:43 +0300

    jhjh

--- stockdb/wsgi.py
+++ stockdb/wsgi.py
@@ -3,11 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockdb.settings')
 
-if os.environ.get("RENDER", None):
-    try:
-        from django.core.management import call_command
-        call_command("migrate", interactive=False)
-    except Exception as e:
-        print("Migration error:", e)
-
 application = get_wsgi_application() 
