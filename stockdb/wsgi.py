3453a096e4f122a10d7812a25320986b00e018c7
emir.dev <emircanbulut04@gmail.com>
2025-06-27 18:55:13 +0300

    cxcx

--- stockdb/wsgi.py
+++ stockdb/wsgi.py
@@ -1,6 +1,15 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockdb.settings')
+
+# Otomatik migrate
+try:
+    django.setup()
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
 
 application = get_wsgi_application() 
